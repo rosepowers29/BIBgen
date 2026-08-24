b5993a83b4286466f01b8d934e8a9266b3631369
Rose Powers <rosepowers@roses-mbp-6.guest-network.net>
2026-08-24 08:49:42 -0500

    fixed absolute path bug

--- examples/training/run_train.sh
+++ examples/training/run_train.sh
@@ -1,8 +1,7 @@
-bash
 #!/bin/bash
 
 export PYTHONPATH=$PYTHONPATH:/opt/conda/lib/python3.10/site-packages
 pip install h5py
 
 export PYTHONPATH=$PWD/src:$PYTHONPATH
-python train.py $1 /config/$4 $2 -e 151 -b 5 -t $3
+python train.py $1 config/$4 $2 -e 151 -b 5 -t $3
