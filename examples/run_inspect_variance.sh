915c41b04d95f756baef500e746052743cbf5a9c
Rose Powers <rosepowers@Roses-MacBook-Pro-6.local>
2026-09-04 09:44:37 -0400

    fixed imports

--- examples/run_inspect_variance.sh
+++ examples/run_inspect_variance.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export PYTHONPATH=$PYTHONPATH:/opt/conda/lib/python3.10/site-packages
+pip install matplotlib
 export PYTHONPATH=$PWD/src:$PYTHONPATH
 python inspect_predicted_variance.py denoiser_$1.pth config/$2 config/$3 -o predicted_variance_$1.png
